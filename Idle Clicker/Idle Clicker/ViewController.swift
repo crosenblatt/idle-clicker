151d0a54e22b4e710cf7982aff9875c12cd98864
crosenblatt <rosench13@gmail.com>
2018-05-31 12:24:11 -0400

    initial commit

---
+++ Idle Clicker/Idle Clicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Idle Clicker
+//
+//  Created by Christopher Rosenblatt on 5/31/18.
+//  Copyright © 2018 crosenblatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
