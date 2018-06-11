9a3cdd31ddfd8788d8f433d58c0a3f7576513618
crosenblatt <rosench13@gmail.com>
2018-06-11 18:00:19 -0400

    add splash screen and incomplete how to play screen

---
+++ Idle Clicker/Idle Clicker/SplashViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SplashViewController.swift
+//  Idle Clicker
+//
+//  Created by Christopher Rosenblatt on 6/11/18.
+//  Copyright © 2018 crosenblatt. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
